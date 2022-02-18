fa5471c8fa8f92e88e6e8d50ae47237dd7b01929
Kacper <k.trzepiecinski@gmail.com>
2022-02-18 19:44:54 +0100

    fix bug fix not connecting to Heroku Postgres

--- db/database.py
+++ db/database.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import re
 
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
