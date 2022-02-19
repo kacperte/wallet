f69a5d17a7139c0929482155643e0595122366db
Kacper <k.trzepiecinski@gmail.com>
2022-02-19 09:48:36 +0100

    update

--- db/database.py
+++ db/database.py
@@ -9,7 +9,7 @@ if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
