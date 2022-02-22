751156e0ae1d3d47b272cefdbe2c36a6e53e52db
Kacper <k.trzepiecinski@gmail.com>
2022-02-22 01:11:03 +0100

    update

--- router/wallet.py
+++ router/wallet.py
@@ -21,7 +21,7 @@ async def create_or_update_database(id: str):
     :param id: wallet adress
     :return: status info
     """
-    result = wallet_reputation.apply_async(args=[id])
+    result = wallet_reputation.delay(id)
 
     return {"message": result}
 
