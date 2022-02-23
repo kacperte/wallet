d4b6d3ada862feafd41ab83bacd04db80d5b3519
Kacper <k.trzepiecinski@gmail.com>
2022-02-23 23:10:58 +0100

    update

--- agents/polygonScraper.py
+++ agents/polygonScraper.py
@@ -108,9 +108,9 @@ class PolygonscanScraper:
             exists = self.session.query(
                 self.session.query(DbNcTransaction)
                 .filter(DbNcTransaction.txn_hash == row[0])
-                .filter(DbNcTransaction.method == row[1])
+                #.filter(DbNcTransaction.method == row[1])
                 .filter(DbNcTransaction.datetime == row[2])
-                # .filter(DbNcTransaction.From == row[3])
+                #  .filter(DbNcTransaction.From == row[3])
                 # .filter(DbNcTransaction.to == row[4])
                 # .filter(DbNcTransaction.quantity == row[5])
                 .exists()
