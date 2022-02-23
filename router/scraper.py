b83da17c730db553abae64184b20de2caaa49d67
Kacper <k.trzepiecinski@gmail.com>
2022-02-24 00:05:16 +0100

    Divide scraper API to 2 function (NC and LP)

--- router/scraper.py
+++ router/scraper.py
@@ -1,17 +1,30 @@
 from fastapi import APIRouter
-from tasks import polygon_scraper
+from tasks import polygon_scraper_lp, polygon_scraper_nc
 
 router = APIRouter(prefix="/scraper", tags=["scraper"])
 
 
-# Connect to polygonscan, collect data and create/update Database
+# Connect to polygonscan, collect data (nc transaction) to create/update Database
 @router.post(
     "/run",
     summary="Scrap polygonscan/NC",
-    description="This API call function scrapping all transaction from NC Coin network.",
+    description="This API call function scrapping all transaction from network.",
     response_description="Message with status",
 )
 async def create_or_update_database():
-    polygon_scraper.delay()
+    polygon_scraper_nc.delay()
+
+    return {"Data": "OK"}
+
+
+# Connect to polygonscan, collect data (lp transaction) to create/update Database
+@router.post(
+    "/run",
+    summary="Scrap polygonscan/LP",
+    description="This API call function scrapping all transaction from network.",
+    response_description="Message with status",
+)
+async def create_or_update_database():
+    polygon_scraper_lp.delay()
 
     return {"Data": "OK"}
--- tasks.py
+++ tasks.py
@@ -17,13 +17,21 @@ app.conf.update(
 )
 
 
-@app.task(name="polygonScraper")
-def polygon_scraper(
-    nc_url: str = "https://polygonscan.com/token/0x64a795562b02830ea4e43992e761c96d208fc58d",
-    lp_url: str = "https://polygonscan.com/token/0x78e16D2fACb80ac536887D1376ACD4EeeDF2fA08",
+@app.task(name="polygonScraper-NC")
+def polygon_scraper_nc(
+    nc_url: str = "https://polygonscan.com/token/0x64a795562b02830ea4e43992e761c96d208fc58d"
 ):
     PolygonscanScraper().scrap_from_url(url=nc_url)
+
+    return {"message": "Success"}
+
+
+@app.task(name="polygonScraper-LP")
+def polygon_scraper_lp(
+    lp_url: str = "https://polygonscan.com/token/0x78e16D2fACb80ac536887D1376ACD4EeeDF2fA08",
+):
     PolygonscanScraper().scrap_from_url(url=lp_url)
+
     return {"message": "Success"}
 
 
