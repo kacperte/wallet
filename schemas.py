c191b3d1395cedfc61eb90272f9d15e17a44fafe
Kacper <k.trzepiecinski@gmail.com>
2022-02-24 03:02:02 +0100

    update

--- schemas.py
+++ schemas.py
@@ -6,7 +6,7 @@ class WalletBase(BaseModel):
     time_in_nc: int = None
     paper_hands: bool = False
     proofs: str = None
-    did_wallet_add_lp: int = None
+    did_wallet_add_lp: bool = None
     how_many_time_add_lp: int = None
     lp_balance: int = None
     nc_balance: int = None
