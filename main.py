00d6ce6ad9ef648d9ace46cf65665d69969c61c2
Kacper <k.trzepiecinski@gmail.com>
2022-03-05 15:43:30 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from db import models
 from db.database import engine
 from router import wallet, scraper
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
 app.include_router(wallet.router)
 app.include_router(scraper.router)
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 def index():
-    return {"message": "NC Wallet Reputation"}
+    return templates.TemplateResponse("main_page.html")
 
 
 models.Base.metadata.create_all(engine)
