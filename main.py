85730d5b34baa16902d51aec4d96cb857fe41b7d
Kacper <k.trzepiecinski@gmail.com>
2022-03-05 20:32:59 +0100

    update

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def index(request: Request, id: str = Form(...)):
-    address = id
     return templates.TemplateResponse(
-        "main_page.html", context={"request": request, "address": address}
+        "main_page.html", context={"request": request, "address": id}
     )
 
 
