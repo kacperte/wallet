87d4ea1a1532acc41be9c83e4f08b9b4c28d5ea6
Kacper <k.trzepiecinski@gmail.com>
2022-03-05 20:36:52 +0100

    update

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def index(request: Request, id: str = Form(...)):
-    return templates.TemplateResponse(
-        "main_page.html", context={"request": request, "address": id}
-    )
+    return templates.TemplateResponse("main_page.html", context={"request": request})
 
 
 models.Base.metadata.create_all(engine)
