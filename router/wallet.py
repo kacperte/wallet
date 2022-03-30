42f01456140a006922eb5af7086c418d88db9406
Kacper <k.trzepiecinski@gmail.com>
2022-03-31 01:29:30 +0200

    update

--- router/wallet.py
+++ router/wallet.py
@@ -42,7 +42,7 @@ async def create_or_update_all():
     :return: status info
     """
     for address in all_addresses_generator():
-        wallet_reputation.delay(address)
+        requests.post(f"https://wallet-reputation.herokuapp.com/wallet/run/{address}")
 
     return {"Status": "Tasks successfully add to execute"}
 
