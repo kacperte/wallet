ec2c27e4a99defcd57bd22bea8f8ed789b5e21a1
Kacper <k.trzepiecinski@gmail.com>
2022-03-31 00:53:33 +0200

    update

--- agents/walletReputation.py
+++ agents/walletReputation.py
@@ -167,6 +167,7 @@ class WalletReputation:
             )
             .text
         )
+        self.driver.close()
 
         # Clean value to final form
         nc_balance = nc_balance.split()[1]
