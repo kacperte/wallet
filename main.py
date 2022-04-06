ca84992ea5ee5083d49ab00b80170cf61fde1e5d
Kacper <k.trzepiecinski@gmail.com>
2022-04-07 01:15:46 +0200

    delete basic view

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI
 from db import models
 from db.database import engine
 from router import wallet, scraper
-from fastapi.templating import Jinja2Templates
-import requests
-
 
 app = FastAPI()
 app.include_router(wallet.router)
 app.include_router(scraper.router)
-templates = Jinja2Templates(directory="templates/")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/", tags=["view"])
-def wallet_view(request: Request):
-    result = "Type a address"
-    return templates.TemplateResponse(
-        "main_page.html", context={"request": request, "result": result}
-    )
-
-
-@app.post("/", tags=["view"])
-def wallet_view(request: Request, address: str = Form(...)):
-    URL = "https://wallet-reputation.herokuapp.com/wallet/"
-    result = requests.get(URL + address).json()
-
-    return templates.TemplateResponse(
-        "main_page.html", context={"request": request, "result": result}
-    )
-
-
 models.Base.metadata.create_all(engine)
