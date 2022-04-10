8b2a3d3e9e0f6fa690c9ecff0f5a41cec83ab09e
Kacper <k.trzepiecinski@gmail.com>
2022-04-10 23:36:19 +0200

    update

--- router/wallet.py
+++ router/wallet.py
@@ -40,7 +40,6 @@ async def create_or_update_all():
     :return: status info
     """
     addresses_list = [address for address in all_addresses_generator()]
-    print(addresses_list)
     wallet_reputation_total.delay(addresses_list)
 
     return {"Status": "Tasks successfully add to execute"}
