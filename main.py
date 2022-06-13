2e8d3bb274c8946b9f21c66a5529585823c97f12
Kacper <k.trzepiecinski@gmail.com>
2022-06-13 23:15:56 +0200

    enable cors

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from router import wallet, scraper
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(wallet.router)
 app.include_router(scraper.router)
 models.Base.metadata.create_all(engine)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
